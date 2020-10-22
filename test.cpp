c21651648c7ebf509cba85cd8f4ce1e0fef7a014
Harendra Chawla <hrchawla87@gmail.com>
2020-10-21 21:34:51 -0700

    dkjsash djhasjhdkj hkjhaskdjsahjkdhkahdhhajshdjkhjkashkdhkjsahjkdh hsajk hdhkas da

--- test.cpp
+++ test.cpp
@@ -3,11 +3,16 @@
 
 using namespace std;
 
+void printSomething(){
+    cout<<"Something"<<endl;
+}
+
 int main()
 {
     vector<int> vin = {1,2,3,4,5};
     for(int in : vin) {
 	in *= 12;
+	in += 10;
 	cout<<in<<endl;
     }
 }
